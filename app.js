c6e0f1c2e441a6c982beeb936524655f6e526a8b
Ssiyam0123 <ssiyam563@gmail.com>
2025-06-13 09:46:35 +0600

    error handaling

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-import {PORT} from './config/env.js'
-import authRouter from './routes/auth.routes.js'
-import userRouter from './routes/user.routes.js'
-import subscriptionRouter from './routes/subscription.routes.js'
-import connectToDatabase from './database/mongodb.js'
+import { PORT } from "./config/env.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
+app.get("/", (req, res) => {
+  res.send("hi i am subscription api");
+});
 
-app.get('/',(req,res)=>{
-    res.send("hi i am subscription api")
-})
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/user', userRouter)
-app.use('/api/v1/subscription', subscriptionRouter)
-
-
-app.listen(PORT,()=>{
-    console.log(`server is running on localhost:${PORT}`)
-    connectToDatabase()
-})
+app.listen(PORT, async () => {
+  console.log(`server is running on localhost:${PORT}`);
+  await connectToDatabase();
+});
